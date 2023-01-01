74c27d48c8246b3b1a28e2f4d62d5fe70ba57f6f
Валентин <123qwemmm@mail.ru>
2023-01-01 18:19:54 +0300

    добавил кнопку фоток Малмыжского ДК

--- markups.py
+++ markups.py
@@ -2,8 +2,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 btnStat = KeyboardButton(text="Статистика МалмыжИнфо", url="https://tgstat.ru/channel/@malmyzh_info/stat")
 btnMap = KeyboardButton(text="Карта Малмыжа", url="https://yandex.ru/maps/20025/malmyzh/?ll=50.684234%2C56.518437&z=15")
+btnDKmalm = KeyboardButton(text="ДК Малмыжа", url="https://disk.yandex.ru/a/Xt6n9vXOnFSQaQ")
 btnUpdate = KeyboardButton("Разбудить Афоню")
 
 mainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 mainMenu.add(btnStat, btnMap)
-mainMenu.add(btnUpdate)
+mainMenu.add(btnDKmalm, btnUpdate)
